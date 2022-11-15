7f6b628c28a5428b1fcd76ab7d907d85f1d3edd1
GustavLW <94694270+GustavLW@users.noreply.github.com>
2022-11-15 17:31:06 +0100

    commit
    rovbajs

--- Diffusion/V.m
+++ Diffusion/V.m
@@ -1,6 +1,8 @@
 function v = V(xi,xj,theta)
-type = 1;
-
+type = 1; % if we want LJ, set to 0. otherwise ignore.
+if length(theta) == 6
+   type = 2; 
+end
 if type == 0
     Vmin = theta(1);
     alpha = theta(2);
@@ -26,6 +28,11 @@ elseif type == 1
      v(1) = (xi(1)-xj(1))/r*vamp;
      v(2) = (xi(2)-xj(2))/r*vamp;
 elseif type == 2
-    
-    
+    v = zeros(2,1);
+    r = max(0.001,norm(xi-xj));
+    p1 = 2*theta(1)*r.*exp(-theta(1)*(r.^2-theta(2)^2))./(1+exp(-theta(1)*(r.^2-theta(2)^2)));
+    p2 = 2*theta(4)*r.*exp(-theta(4)*(r.^2-theta(5)^2))./(1+exp(-theta(4)*(r.^2-theta(5)^2)));
+    vamp = theta(3)*p1 - theta(6)*(p2-p1);
+    v(1) = (xi(1)-xj(1))/r*vamp;
+    v(2) = (xi(2)-xj(2))/r*vamp;
 end
--- Diffusion/wasserstein_gamma.m
+++ Diffusion/wasserstein_gamma.m
@@ -1,4 +1,4 @@
 function W = wasserstein_gamma(a,b,sigma0)
 
 F1 = @(x) abs(gamcdf(x,a,b) - heaviside(x-sigma0));
-W = integral(F1,0,inf);
+W = integral(F1,0,inf)/sigma0;
